c4e07e7620c46aebee17afc689af7b02fd6c90ee
PavelVoytsovich <pavel@pubgenius.io>
2021-04-24 21:39:58 +0300

    update compose and add jenkins jobs

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ COPY pyconsul_task.py /app
 
 # Install needed stuff
 RUN pip install --no-cache-dir python-consul 
-EXPOSE 5000
+EXPOSE 8000
 CMD ["python3", "/app/pyconsul_task.py"]
--- pyconsul_task.py
+++ pyconsul_task.py
@@ -11,28 +11,53 @@ Example usage for inject:
     https://github.com/google/styleguide/blob/gh-pages/pyguide.md
 """
 
-__version__ = '1.0'
 __author__ = 'Pavel Voytsovich'
+__version__ = '1.0'
+__forked_from__ = "https://gist.github.com/mdonkers"
+
 
 
 import consul
+import logging
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-def terraform_inject(service_name, secrets):
-    """Rendering  understandable secrets file"""
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-    with open("te/" + "ololo.", 'w+') as out:
-        for secret in filter(params_filter, secrets):
-            print("Exporting:", secret['Name'])
-        out.write('}\n')
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
-def main():
-    """Main function, calling inject by default, or migrate env if specified"""
-    shell_args = get_parser()
-    service_name = shell_args.service
-    prefix = "/%s/%s" % (shell_args.env, shell_args.service)
-    terraform_inject(service_name, secrets)
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), post_data.decode('utf-8'))
 
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
-if __name__ == "__main__":
-    main()
+def run(server_class=HTTPServer, handler_class=S, port=8000):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
